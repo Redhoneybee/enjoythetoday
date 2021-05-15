a54921c77bd3d1b2a9ee7c620082658fe0f6349e
coderB <flack319@gmail.com>
2021-05-15 21:18:42 +0900

    update bar:re

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,7 +2,16 @@ import React from "react";
 
 const Info = () => {
     return (
-        <div className="flex info" style={{ width: "100%", height: 80, backgroundColor: "white" }}>
+        <div
+            className="flex info"
+            style={{
+                position: "fixed",
+                bottom: 0,
+                left: 0,
+                width: "100%",
+                height: 80,
+                backgroundColor: "white"
+            }}>
             <span className="update">업데이트 : version 0.0.3</span>
             <span className="update">게임 정확하고 빠르게 UI 조정, 이미지 랜더링 개선</span>
         </div>
